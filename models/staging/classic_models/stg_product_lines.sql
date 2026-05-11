8e4d55c8fb5ee49b0edad2dfc7b2e09cbe75dcd4
Richard McEnery <rmcenery@phdata.io>
2026-05-11 22:22:00 +0000

    first cut of stage build out

---
+++ models/staging/classic_models/stg_product_lines.sql
@@ -0,0 +1,19 @@
+with source as (
+
+    select *
+    from {{ source('classic_models', 'product_lines') }}
+
+), renamed as (
+
+    select
+        product_line,
+        text_description,
+        html_description,
+        image,
+        _sync_date
+    from source
+
+)
+
+select *
+from renamed
